350ef651295c10e447c5c70ba9bbdbd3d6a9e8d0
Xiao Shengguang <shengguangxiao@gmail.com>
2022-04-05 11:24:40 +0800

    Filter v2 stakers for NFT airdrop
    Filter v2 stakers for NFT airdrop

--- test/FilterStakers.js
+++ test/FilterStakers.js
@@ -3,13 +3,13 @@ var fs = require('fs');
 
 const { BigNumber } = require('bignumber.js');
 
-var jsonFile = "./build/contracts/GamersePool.json";
+var jsonFile = "./artifacts/contracts/Staking.sol/GamersePool.json";
 var parsed = JSON.parse(fs.readFileSync(jsonFile));
 var abi = parsed.abi;
 
 const testWeb3 = new Web3("https://bsc-dataseed4.binance.org/");
 
-const stakingInst = new testWeb3.eth.Contract(abi, "0x1ac7808b112FfF1bAf47aBd65cB321A6044Bbe1D");
+const stakingInst = new testWeb3.eth.Contract(abi, "0xCac66Dd9ffe2a77B3b0025856dDCB40Dad37B420");
 stakingInst.methods.startBlock().call(function (err, result) {
    console.log(result);
 });
@@ -18,9 +18,12 @@ let stakersMap = new Map();
 
 const threshold = new BigNumber(1e22);
 
+const fromBlock = 16397469;
+const toBlock = fromBlock + 5000;
+
 stakingInst.getPastEvents('Deposit', {
-   fromBlock: 14300586,
-   toBlock: 14304850
+   fromBlock: fromBlock,
+   toBlock:   toBlock
 }, function (error, events) {
    console.log("Length ", events.length);
    for(let i = 0; i < events.length; ++i) {
@@ -29,7 +32,21 @@ stakingInst.getPastEvents('Deposit', {
    const amount = new BigNumber(event["returnValues"]["amount"]);
    const block = event["blockNumber"];
    if (!stakersMap.has(user) && amount.isGreaterThan(threshold)) {
-      console.log("User,",user,",amount,", event["returnValues"]["amount"],",start_block,",block);
+      console.log("User Deposit,",user,",amount,", event["returnValues"]["amount"],",start_block,",block);
       stakersMap.set(user, block);
    }
+} });
+
+console.log("Filter withdraw");
+stakingInst.getPastEvents('Withdraw', {
+   fromBlock: fromBlock,
+   toBlock:   toBlock
+}, function (error, events) {
+   console.log("Length ", events.length);
+   for(let i = 0; i < events.length; ++i) {
+   const event = events[i];
+   const user = event["returnValues"]["user"];
+   const amount = new BigNumber(event["returnValues"]["amount"]);
+   const block = event["blockNumber"];
+   console.log("User Withdraw,",user,",amount,", event["returnValues"]["amount"],",start_block,",block);
 } });
