43861832666bdcfb1692c7357586ee48fb9928e1
Xiao Shengguang <shengguangxiao@gmail.com>
2022-05-16 22:18:13 +0800

    Change parameters to deploy on polygon mumbai testnet

--- args/LFGTokenArg.js
+++ args/LFGTokenArg.js
@@ -2,6 +2,6 @@ module.exports = [
     "LFG Token",
     "LFG",
     "1000000000000000000000000000", // Initial supply
-    "0xbea6eaa4d05a9324fe19f7bdd48ec5bad3895ffc" // Owner
+    "0x3ca3822163D049364E67bE19a0D3B2F03B7e99b5" // Owner
   ];
   
--- deploy/4_lfg_staking.ts
+++ deploy/4_lfg_staking.ts
@@ -6,17 +6,17 @@ async function deploy() {
     "GamersePool"
   );
   const gamersePool: Contract = await GamersePool.deploy(
-    "0xF93f6b686f4A6557151455189a9173735D668154", // process.env.LP_TOKEN_ADDRESS",
-    "0xF93f6b686f4A6557151455189a9173735D668154", // process.env.LFG_TOKEN_ADDRESS,
-    "0xbea6Eaa4D05A9324fE19f7bDD48ec5BaD3895fFC", // process.env.REWARD_HOLDER_ADDRESS,
-    "0xd1DED8e429cC944e8a14cFcc1ed9286BE3E856E7", // process.env.CUSTODY_ADDRESS,
-    "3474635163300000000",
-    "16399348",
-    "18126148",
+    "0x82cEFec0784fDDe34396C57472A717D1e7f5506B", // process.env.LP_TOKEN_ADDRESS",
+    "0x82cEFec0784fDDe34396C57472A717D1e7f5506B", // process.env.LFG_TOKEN_ADDRESS,
+    "0xf197c5bC13383ef49511303065d39b33DC063f72", // process.env.REWARD_HOLDER_ADDRESS,
+    "0xf197c5bC13383ef49511303065d39b33DC063f72", // process.env.CUSTODY_ADDRESS,
+    "833333333333333000",
+    "26363681",
+    "28763681",
     "5000",
     "1728000",
-    "10000000000000000000000",
-    "201460"
+    "20000000000000000000000",
+    "280000"
   );
   await gamersePool.deployed();
 
